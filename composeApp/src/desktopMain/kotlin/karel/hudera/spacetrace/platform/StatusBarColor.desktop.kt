2460812ec7721237c2dfa49bebae405d3d1bb652
Scar <karel.hudera@gmail.com>
2024-10-31 23:02:32 +0100

    feat: theme and status bar color

---
+++ composeApp/src/desktopMain/kotlin/karel/hudera/spacetrace/platform/StatusBarColor.desktop.kt
@@ -0,0 +1,8 @@
+package karel.hudera.spacetrace.platform
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun StatusBarColor(isDark: Boolean) {
+    // TODO
+}
---
+++ composeApp/src/iosMain/kotlin/karel/hudera/spacetrace/platform/StatusBarColor.ios.kt
@@ -0,0 +1,17 @@
+package karel.hudera.spacetrace.platform
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import platform.UIKit.UIApplication
+import platform.UIKit.UIStatusBarStyleDarkContent
+import platform.UIKit.UIStatusBarStyleLightContent
+import platform.UIKit.setStatusBarStyle
+
+@Composable
+internal actual fun StatusBarColor(isDark: Boolean) {
+    LaunchedEffect(isDark) {
+        UIApplication.sharedApplication.setStatusBarStyle(
+            if (isDark) UIStatusBarStyleDarkContent else UIStatusBarStyleLightContent
+        )
+    }
+}
