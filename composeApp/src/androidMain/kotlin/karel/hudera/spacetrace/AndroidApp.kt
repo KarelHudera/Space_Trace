0e7d6dc095b58acc02fe81d200681aea28a22e15
Scar <karel.hudera@gmail.com>
2024-11-05 21:51:18 +0100

    feat: init Koin

--- composeApp/src/androidMain/kotlin/karel/hudera/spacetrace/AndroidApp.kt
+++ composeApp/src/androidMain/kotlin/karel/hudera/spacetrace/AndroidApp.kt
@@ -1,6 +1,12 @@
 package karel.hudera.spacetrace
 
 import android.app.Application
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import karel.hudera.spacetrace.di.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
 
 class AndroidApp : Application() {
     companion object {
@@ -9,10 +15,12 @@ class AndroidApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-//        initKoin {
-//            androidLogger(if (isDebug()) Level.ERROR else Level.NONE)
-//            androidContext(this@AndroidApp)
-//        }
+        initKoin {
+            androidLogger(if (isDebug()) Level.ERROR else Level.NONE)
+            androidContext(this@AndroidApp)
+        }
         INSTANCE = this
     }
+
+    private fun Context.isDebug() = 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
 }
--- composeApp/src/wasmJsMain/kotlin/karel/hudera/spacetrace/main.kt
+++ composeApp/src/wasmJsMain/kotlin/karel/hudera/spacetrace/main.kt
@@ -2,10 +2,13 @@ package karel.hudera.spacetrace
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import karel.hudera.spacetrace.di.initKoin
 import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoin {}
+
     ComposeViewport(document.body!!) {
         App()
     }
