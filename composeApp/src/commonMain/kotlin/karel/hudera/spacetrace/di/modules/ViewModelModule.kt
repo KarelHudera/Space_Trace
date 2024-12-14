905cf9bf8acb804f51473a6e8581f27e7cf1e8f2
Scar <karel.hudera@gmail.com>
2024-12-14 15:52:29 +0100

    feat: Database use cases

--- composeApp/src/commonMain/kotlin/karel/hudera/spacetrace/di/modules/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/karel/hudera/spacetrace/di/modules/ViewModelModule.kt
@@ -1,8 +1,10 @@
 package karel.hudera.spacetrace.di.modules
 
+import karel.hudera.spacetrace.presentation.ui.features.articleDetail.ArticleDetailViewModel
 import karel.hudera.spacetrace.presentation.ui.features.news.NewsScreenViewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     factory { NewsScreenViewModel(get(), get()) }
+    factory { params -> ArticleDetailViewModel(get(), get(), get(), params.get()) }
 }
--- composeApp/src/commonMain/kotlin/karel/hudera/spacetrace/domain/interactors/IsArticleFavoriteUseCase.kt
+++ composeApp/src/commonMain/kotlin/karel/hudera/spacetrace/domain/interactors/IsArticleFavoriteUseCase.kt
@@ -2,12 +2,11 @@ package karel.hudera.spacetrace.domain.interactors
 
 import karel.hudera.spacetrace.domain.IRepository
 import karel.hudera.spacetrace.domain.interactors.type.BaseUseCase
-import karel.hudera.spacetrace.domain.model.Article
 import kotlinx.coroutines.CoroutineDispatcher
 
-class GetArticleUseCase(
+class IsArticleFavoriteUseCase(
     private val repository: IRepository,
     dispatcher: CoroutineDispatcher,
-) : BaseUseCase<Int, Article>(dispatcher){
-    override suspend fun block(param: Int): Article = repository.getArticle(param)
+) : BaseUseCase<Int, Boolean>(dispatcher) {
+    override suspend fun block(param: Int): Boolean = repository.isArticleFavorite(param)
 }
